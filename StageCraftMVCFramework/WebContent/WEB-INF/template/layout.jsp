c0dc288c4cd4b5644b8eadc835a370417050587a
Eden <emelga00@uafs.edu>
2013-09-21 10:47:11 -0500

    update

--- StageCraftMVCFramework/WebContent/WEB-INF/template/layout.jsp
+++ StageCraftMVCFramework/WebContent/WEB-INF/template/layout.jsp
@@ -1,32 +1,74 @@
-<%@ page contentType="text/html; charset=iso-8859-1" language="java"
-import="java.util.*,java.sql.*,data.*,beans.*" %>
-<%
-   //*** Prevent Browser Caching of Pages *****
-   response.setHeader("Pragma", "No-cache");
-   response.setDateHeader("Expires", 0);
-   response.setHeader("Cache-Control", "no-cache");
+<%@ page contentType="text/html; charset=iso-8859-1" language="java" import="java.util.*,java.sql.*,data.*,beans.*" %>
 
-   String content =  (String) request.getAttribute("content");
-  
+<%
+/****************
+ *  Author: JLH
+ ****************/
+response.setHeader("Pragma", "No-cache");
+response.setDateHeader("Expires", 0);
+response.setHeader("Cache-Control", "no-cache");
+
+String nextView =  (String) request.getAttribute("nextView");
+String urlMapping = (String)session.getAttribute("urlMapping");
+
 %>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-   
-      <title>MVC Students</title>
-      <link rel="stylesheet" href="<%=request.getContextPath()%>/style.css" type="text/css"></link>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+   <head>
+      <title>Enterprise Java - Group1 Project</title>
+      <link rel="stylesheet" href="<%=request.getContextPath()%>/styles/style.css" type="text/css"></link>
    </head>
+   
    <body>
-       <header>
-          <jsp:include page="/WEB-INF/template/header.jsp" />
-       </header>
-       <section>
-	      <jsp:include page="<%=content%>"  />
-	   </section>
-	   <footer>
-	      <jsp:include page="/WEB-INF/template/footer.jsp" />
-	   </footer>	       	       
-  </body>
+   
+       <div id="container">
+       
+       
+           <div id="clearfooter">
+		       
+		       
+				<div id="header">
+			    	<!-- Header  -->
+					<%
+						if(urlMapping.equals("/CAClientListView")||urlMapping.equals("/CAInfoListView")||
+						urlMapping.equals("/CACredentialsListView")||urlMapping.equals("/ClientViewerList")||
+						urlMapping.equals("/HrEmployeeListView")||urlMapping.equals("/HrSystemUserListView")){
+					%>
+						<jsp:include page="/WEB-INF/includes/header2.jsp" />
+					<%
+					} else {
+					%>
+						<jsp:include page="/WEB-INF/includes/header.jsp" />
+					<%
+					}
+					%>
+				</div><!-- END header-->
+		       
+		       
+				<div id="content">
+		            <!-- Page Content -->
+		        	<jsp:include page="<%=nextView%>"  />
+				</div><!-- END content-->
+			       
+			       
+			</div><!-- END clearfooter-->
+		       
+		       
+   			<div id="clearfooter2">
+   			</div><!-- END clearfooter2-->
+      
+       
+			<div id="footer">
+		        	<!-- Footer -->
+		        	<jsp:include page="/WEB-INF/includes/footer.jsp" />
+			</div><!-- END footer-->
+
+
+       </div><!-- END container-->
+       
+   </body>
+   
 </html>
 
--- StageCraftMVCFramework/src/beans/User.java
+++ StageCraftMVCFramework/src/beans/User.java
@@ -1,42 +1,103 @@
 package beans;
-
+/***************
+ *  Author: JLH
+ ***************/
 public class User {
-	private int userID;
-	private String userName;
-	private String password;
-	private String lastName;
-	private String firstName;
+	//from userDB
+	private int    user_ID;
+	private String first_Name;
+	private String last_Name;
+	private String Phone;
+	private String Address;
+	private String City;
+	private String State;
+	private String ZIP;
+	private String Date;
+	private String Role;
+	//from credentialDB
+	private int    creds_Credential_ID;
+	private String creds_Email;
+	private String creds_Pass;
 	
-	public int getUserID() {
-		return userID;
+	//from userDB
+	public int getUser_ID() {
+		return user_ID;
 	}
-	public void setUserID(int userID) {
-		this.userID = userID;
+	public void setUser_ID(int user_ID) {
+		this.user_ID = user_ID;
 	}
-	public String getUserName() {
-		return userName;
+	public String getFirst_Name() {
+		return first_Name;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setFirst_Name(String first_Name) {
+		this.first_Name = first_Name;
 	}
-	public String getPassword() {
-		return password;
+	public String getLast_Name() {
+		return last_Name;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+	public void setLast_Name(String last_Name) {
+		this.last_Name = last_Name;
 	}
-	public String getLastName() {
-		return lastName;
+	public String getPhone() {
+		return Phone;
 	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setPhone(String phone) {
+		Phone = phone;
 	}
-	public String getFirstName() {
-		return firstName;
+	public String getAddress() {
+		return Address;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setAddress(String address) {
+		Address = address;
 	}
-	
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getZIP() {
+		return ZIP;
+	}
+	public void setZIP(String zIP) {
+		ZIP = zIP;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public String getRole() {
+		return Role;
+	}
+	public void setRole(String role) {
+		Role = role;
+	}
+	//from credentialDB
+	public int getCreds_Credential_ID() {
+		return creds_Credential_ID;
+	}
+	public void setCreds_Credential_ID(int creds_Credential_ID) {
+		this.creds_Credential_ID = creds_Credential_ID;
+	}
+	public String getCreds_Email() {
+		return creds_Email;
+	}
+	public void setCreds_Email(String creds_email) {
+		this.creds_Email = creds_email;
+	}
+	public String getCreds_Pass() {
+		return creds_Pass;
+	}
+	public void setCreds_Pass(String creds_pass) {
+		this.creds_Pass = creds_pass;
+	}
 	
 }
